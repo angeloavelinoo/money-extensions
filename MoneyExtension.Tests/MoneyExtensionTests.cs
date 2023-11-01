09b94f4e04d7a9a3692b7b3abbc68217bc0d561f
Angelo Avelino <angeloavelino33211781@gmail.com>
2023-10-31 22:52:57 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,12 @@
+namespace MoneyExtension.Tests;
+
+[TestClass]
+public class MoneyExtensionTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.8M;
+        var cents = valor.ToCents();
+    }
+}
